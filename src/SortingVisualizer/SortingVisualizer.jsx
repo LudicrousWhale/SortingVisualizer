dbedd9f1dd0877c2cba9737f6d99639b4b87452a
LudicrousWhale <mshivam2012@gmail.com>
2021-03-30 22:52:11 +0530

    Added Merge Sort

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import {getMergeSortAnimations} from '../SortingAlgorithms/SortingAlgorithms.js';
+import './SortingVisualizer.css';
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 2;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 310;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'mediumseagreen';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'black';
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            array: [],
+        };
+    }
+
+    componentDidMount() {
+        this.resetArray();
+    }
+
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < 310; i++) {
+            array.push(randIntFromInterval(5, 900));
+        }
+        this.setState({array});
+    }
+
+    mergeSort() {
+        const animations = getMergeSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+          const arrayBars = document.getElementsByClassName('array-bar');
+          const isColorChange = i % 3 !== 2;
+          if (isColorChange) {
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+            setTimeout(() => {
+              barOneStyle.backgroundColor = color;
+              barTwoStyle.backgroundColor = color;
+            }, i * ANIMATION_SPEED_MS);
+          } else {
+            setTimeout(() => {
+              const [barOneIdx, newHeight] = animations[i];
+              const barOneStyle = arrayBars[barOneIdx].style;
+              barOneStyle.height = `${newHeight}px`;
+            }, i * ANIMATION_SPEED_MS);
+          }
+        }
+      }
+    
+
+    render() {
+        const {array} = this.state;
+
+        return (
+            <div className="array-container">
+                {array.map((value, idx) => (
+                    <div 
+                        className="array-bar" 
+                        key={idx}
+                        style={{height: `${value}px`}}
+                        >
+                    </div>
+                ))}
+                <button onClick={() => this.resetArray()}className="generate">Generate New Array</button> 
+                <button onClick={() => this.mergeSort()}className="generate-merge">Merge Sort</button>
+            </div>
+        );
+    }
+}
+
+function randIntFromInterval(min, max) {
+    return Math.floor(Math.random() *  (max - min + 1) + min);
+}
